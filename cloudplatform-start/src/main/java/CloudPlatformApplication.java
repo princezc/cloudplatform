01f60c5c17c2d76285fc047168ff9a9fbb4f581f
836243070@qq.com <836243070@qq.com>
2018-09-13 20:35:50 +0800

    骨架搭建

--- cloudplatform-start/src/main/java/CloudPlatformApplication.java
+++ cloudplatform-start/src/main/java/CloudPlatformApplication.java
@@ -1,7 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @ClassName CloudPlatformApplication
+ * @Author zhengcheng
+ * @Date 2018/9/13 20:33
+ **/
+@SpringBootApplication
 public class CloudPlatformApplication {
     public static void main(String[] args) {
-
-
+        SpringApplication.run(CloudPlatformApplication.class, args);
     }
 }
